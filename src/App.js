2099cdda9ac357b0f1460c166a1d037095b8b19c
Hardayal Punshi <arorap876@gmail.com>
2023-03-10 05:40:43 +1000

    adding error handling model

--- src/App.js
+++ src/App.js
@@ -6,10 +6,19 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   function fetchMoviesHandler() {
     setLoading(true);
+    setError(null);
     fetch("https://swapi.dev/api/films")
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Something went wrong");
+        }
+
+        return response.json();
+      })
       .then((data) => {
         const transformMovies = data.results.map((movie) => {
           return {
@@ -21,7 +30,10 @@ function App() {
         });
         setMovies(transformMovies);
         setLoading(false);
-      });
+      })
+      .catch((error) => setError(error.message));
+
+    setLoading(false);
   }
 
   // async function fetchMoviesHandler(){
@@ -46,8 +58,11 @@ function App() {
       </section>
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <h1>Found no movies!!</h1>}
+        {!isLoading && movies.length === 0 && !error && (
+          <h1>Found no movies!!</h1>
+        )}
         {isLoading && <h1>Loading...</h1>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
