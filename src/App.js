c9dd778183eb69acf81309f40592ebb85af799ab
Hardayal Punshi <arorap876@gmail.com>
2023-03-10 21:35:59 +1000

    adding async function

--- src/App.js
+++ src/App.js
@@ -8,62 +8,80 @@ function App() {
   const [isLoading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  function fetchMoviesHandler() {
+  // function fetchMoviesHandler() {
+  //   setLoading(true);
+  //   setError(null);
+  //   fetch("https://swapi.dev/api/films")
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error("Something went wrong");
+  //       }
+
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       const transformMovies = data.results.map((movie) => {
+  //         return {
+  //           id: movie.episode_id,
+  //           title: movie.title,
+  //           openingText: movie.opening_crawl,
+  //           releaseDate: movie.release_date,
+  //         };
+  //       });
+  //       setMovies(transformMovies);
+  //       setLoading(false);
+  //     })
+  //     .catch((error) => setError(error.message));
+
+  //   setLoading(false);
+  // }
+
+  async function fetchMoviesHandler() {
     setLoading(true);
     setError(null);
-    fetch("https://swapi.dev/api/films")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Something went wrong");
-        }
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+      const data = await response.json();
 
-        return response.json();
-      })
-      .then((data) => {
-        const transformMovies = data.results.map((movie) => {
-          return {
-            id: movie.episode_id,
-            title: movie.title,
-            openingText: movie.opening_crawl,
-            releaseDate: movie.release_date,
-          };
-        });
-        setMovies(transformMovies);
-        setLoading(false);
-      })
-      .catch((error) => setError(error.message));
-
+      const transformMovies = data.results.map((movie) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date,
+        };
+      });
+      setMovies(transformMovies);
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
+    }
     setLoading(false);
   }
 
-  // async function fetchMoviesHandler(){
-  //   const response=await fetch("https://swapi.dev/api/films");
-  //   const data= await response.json()
+  let content = <h1>Please Click the above button!!</h1>;
 
-  //   const transformMovies = data.results.map((movie) => {
-  //     return {
-  //       id: movie.episode_id,
-  //       title: movie.title,
-  //       openingText: movie.opening_crawl,
-  //       releaseDate: movie.release_date,
-  //     };
-  //   });
-  //   setMovies(transformMovies);
-  // }
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <h1>Loading Your Movies...</h1>;
+  }
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && !error && (
-          <h1>Found no movies!!</h1>
-        )}
-        {isLoading && <h1>Loading...</h1>}
-        {!isLoading && error && <p>{error}</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
